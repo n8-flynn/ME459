1b2487b2563db2045b49b8001efb0f5055d988da
Dan Negrut <negrut@wisc.edu>
2020-10-27 12:51:28 -0500

    one more mistake added to the code; it gets caught by the compiler, w/ -Wall

--- code/Debugging/sanitizeExample.c
+++ code/Debugging/sanitizeExample.c
@@ -10,5 +10,10 @@ int sum(int* x, int len) {
 
 int main() {
     int x[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-    printf("sum = %d\n", sum(x, 9));
+    int mySum = sum(x, 9);
+    printf("sum = %d\n", mySum);
+    int b;
+    mySum += b;
+    printf("Just added b to sum:%d\n", mySum);
+    return 0;
 }
