0538b60bc5c9e192e866b3499892ab81790ad690
Luning Fang <lfang9@wisc.edu>
2021-11-03 17:44:22 -0500

    source code for HW07

---
+++ assignments/2021/HW07/alloc.c
@@ -0,0 +1,8 @@
+#include "alloc.h"
+
+// allocate memory and populate the array of Record
+void initializeStudentData(Record **ptr, int numStudents){
+    /*********** Implementation of this function goes here ***********/
+
+
+}
---
+++ assignments/2021/HW07/task2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "record.h"
+#include "alloc.h"
+#include "mysort.h"
+
+
+// print out an array of Record in format: firstname MI lastname, gpa, MM-DD-YY
+void printRecords(Record *ptr, int numStudents){
+    /*********** Implementation of this function goes here ***********/
+}
+
+int main(int argc, char* argv[]){
+    if (argc != 2){
+        printf("provide number of students\n");
+        return 1;
+    }
+
+    /*********** call methods to allocate memory, populate an array of Record, sort, print and free the memory ***********/
+
+    return 0;
+}
