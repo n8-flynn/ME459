7faaa46ecd8de72e81279949a0452e93eeaa5d45
Luning Fang <lfang9@wisc.edu>
2021-11-04 09:55:17 -0500

    add mysort.c

---
+++ assignments/2021/HW07/mysort.c
@@ -0,0 +1,15 @@
+int compareLastName(const void*a, const void*b) {
+    /*********** Implementation of this function goes here ***********/
+
+}
+
+int compareGPA(const void*a, const void*b) {
+    /*********** Implementation of this function goes here ***********/
+
+}
+
+int compareDOB(const void*a, const void*b) {
+    /*********** Implementation of this function goes here ***********/
+
+
+}
