f6cbbe69f66bc4713020de515f02a37b25c4c38a
LuningFang <lfang9@wisc.edu>
2021-11-04 09:48:14 -0500

    Update alloc.c

--- assignments/2021/HW07/alloc.c
+++ assignments/2021/HW07/alloc.c
@@ -1,6 +1,6 @@
 #include "alloc.h"
 
-// allocate memory and populate the array of Record
+// allocate memory using malloc and populate the array of Record
 void initializeStudentData(Record **ptr, int numStudents){
     /*********** Implementation of this function goes here ***********/
 
